d883dcc9ce8d1472e8dff6fd7690cb241ae5e7cb
Sean McGinty <newfolderlocation@gmail.com>
2025-05-09 15:37:56 +0800

    Modernize code, skip set interval if already ended, adjust formatting on 0 second to 0 seconds etc

--- assets/countdown.js
+++ assets/countdown.js
@@ -1,42 +1,54 @@
 (() => {
-  var now = Date.now();
+  const now = Date.now();
 
-  var f = ((element, seconds, prefix) => {
+  const f = ((element, seconds, prefix) => {
     if (seconds <= 0) {
       element.innerHTML = '&nbsp;';
       return;
     }
-    var days = (seconds / 86400)|0;
-    var hours = ((seconds % 86400) / 3600)|0;
-    var minutes = ((seconds % 3600) / 60)|0;
-    var seconds = seconds % 60;
+    const days = (seconds / 86400) | 0;
+    const hours = ((seconds % 86400) / 3600) | 0;
+    const minutes = ((seconds % 3600) / 60) | 0;
+    seconds = seconds % 60;
 
     if (days > 0) {
-      element.textContent = prefix + days + ' day' + (days > 1 ? 's' : '') +
-          ', ' + hours + ' hour' + (hours > 1 ? 's' : '');
+      element.textContent = prefix + days + ' day' + (days != 1 ? 's' : '') +
+          ', ' + hours + ' hour' + (hours != 1 ? 's' : '');
     } else if (hours > 0) {
-      element.textContent = prefix + hours + ' hour' + (hours > 1 ? 's' : '') +
-          ', ' + minutes + ' minute' + (minutes > 1 ? 's' : '');
+      element.textContent = prefix + hours + ' hour' + (hours != 1 ? 's' : '') +
+          ', ' + minutes + ' minute' + (minutes != 1 ? 's' : '');
     } else if (minutes > 0) {
-      element.textContent = prefix + minutes + ' minute' + (minutes > 1 ? 's' : '') +
-          ', ' + seconds + ' second' + (seconds > 1 ? 's' : '');
+      element.textContent = prefix + minutes + ' minute' + (minutes != 1 ? 's' : '') +
+          ', ' + seconds + ' second' + (seconds != 1 ? 's' : '');
     } else {
-      element.textContent = prefix + seconds + ' second' + (seconds > 1 ? 's' : '');
+      element.textContent = prefix + seconds + ' second' + (seconds != 1 ? 's' : '');
     }
   });
 
+  const elapsed = ((Date.now() - now) / 1000) | 0;
+  const startIn = document.getElementsByName("start_in")[0].content - elapsed;
+  const endsIn = document.getElementsByName("ends_in")[0].content - elapsed;
+
+  if (startIn <= 0 && endsIn <= 0) {
+    const elements = document.getElementsByClassName('ctfd-event-countdown');
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].innerHTML = '&nbsp;';
+    }
+    return;
+  }
+
   setInterval(() => {
-    var elapsed = ((Date.now() - now)/1000)|0;
-    var elements = document.getElementsByClassName('ctfd-event-countdown');
-    for (var i=0; i<elements.length; i++) {
-      var element = elements[i];
-      var seconds = document.getElementsByName("start_in")[0].content - elapsed;
+    const elapsed = ((Date.now() - now) / 1000) | 0;
+    const elements = document.getElementsByClassName('ctfd-event-countdown');
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      let seconds = document.getElementsByName("start_in")[0].content - elapsed;
       if (seconds > 0) {
-        f(element, seconds, "Event starts in ");
+        f(element, seconds, "Starts in ");
       } else {
         seconds = document.getElementsByName("ends_in")[0].content - elapsed;
-        f(element, seconds, "Event ends in ");
+        f(element, seconds, "Ends in ");
       }
     }
   }, 1000);
-})()
+})();
